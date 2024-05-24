f162c4e56a715c0443fde6954b065b4ce35823c2
Swapnita Singh <swapimamuni@gmail.com>
2024-05-24 11:56:06 +0530

    nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
+      <BrowserRouter>
+      <Routes>
+      
+      </Routes>
+      </BrowserRouter>
       
     </div>
   )
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Tours</li>
+                
+            </ul>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
