86422e95b7463580ba9d1261ba21d3db37d2003b
Swapnita Singh <swapimamuni@gmail.com>
2024-05-24 14:14:29 +0530

    navdone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Tour from './pages/Tour';
+import './App.css';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
       <BrowserRouter>
+      <Navbar/>
       <Routes>
-      
+      <Route path="/home" element={<Home/>} />
+      <Route path='/about' element={<About/>}/>
+      <Route path='/tour' element={< Tour/>}/>
       </Routes>
       </BrowserRouter>
       
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+
   return (
-    <div className='nav'>
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Tours</li>
-                
+    <div className='nav' >
+        <nav className='nav-container'>
+            <div>
+                <h1>TRAVROAM</h1>
+            </div>
+            <div className='nav-links'>
+            <ul className={`menu ${isOpen ? 'open' : ''}`}>
+           <li><Link to='/home'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/tour'>Tour</Link></li>
             </ul>
+            </div>
+            <div className='hamburger' onClick={toggleMenu}>
+          &#9776; {/* Hamburger icon */}
+          </div>
         </nav>
       
     </div>
